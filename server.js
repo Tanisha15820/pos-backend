7c5243140f17bfe54cd1b5725f52ce935aed832c
rohitarora <rohitarora15820@gmail.com>
2025-08-22 09:15:52 +0530

    fix api issue

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use("/api/invoices", invoiceRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-setInterval(async () => {
-  try {
-    await fetch(`https://pos-backend-6lmf.onrender.com/`);
-    console.log("Pinged self to stay awake at", new Date());
-  } catch (err) {
-    console.error("Ping failed", err);
-  }
-}, 5 * 60 * 1000); // every 5 minutes
+// setInterval(async () => {
+//   try {
+//     await fetch(`https://pos-backend-6lmf.onrender.com/`);
+//     console.log("Pinged self to stay awake at", new Date());
+//   } catch (err) {
+//     console.error("Ping failed", err);
+//   }
+// }, 5 * 60 * 1000); // every 5 minutes
