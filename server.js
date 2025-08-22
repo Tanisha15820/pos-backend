ec5004294f5f59c714b50128c2413c6768bc0b6b
rohitarora <rohitarora15820@gmail.com>
2025-08-22 08:39:39 +0530

    Added Period Interval

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const fetch = require("node-fetch"); // npm install node-fetch
 
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/products");
@@ -20,3 +21,12 @@ app.use("/api/invoices", invoiceRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+setInterval(async () => {
+  try {
+    await fetch(`https://pos-backend-6lmf.onrender.com/`);
+    console.log("Pinged self to stay awake at", new Date());
+  } catch (err) {
+    console.error("Ping failed", err);
+  }
+}, 5 * 60 * 1000); // every 5 minutes
